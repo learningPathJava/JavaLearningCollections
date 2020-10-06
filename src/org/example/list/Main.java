20ef4de0e23d2ba5fdc1c8cc66cf25a68866da7d
Violeta <36820411+VioDVC@users.noreply.github.com>
2020-10-06 15:09:31 +0300

    update

--- src/org/example/list/Main.java
+++ src/org/example/list/Main.java
@@ -1,9 +1,8 @@
-package eu.learn.list;
+package org.example.list;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
